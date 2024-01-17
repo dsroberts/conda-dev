d1401d6444ebffb26463a3ec641f588ec10da4e7
Dale Roberts <dsroberts@unimelb.edu.au>
2024-01-17 13:03:05 +1100

    Make array configs optional

--- environments/esmvaltool/config.sh
+++ environments/esmvaltool/config.sh
@@ -1,8 +1,7 @@
 ### config.sh MUST provide the following:
-### $ENVIRONMENT
 ### $FULLENV
 ###
-### Arrays (can be empty)
+### Arrays (optional, can be empty)
 ### rpms_to_remove
 ### replace_from_apps
 ### outside_commands_to_include
@@ -16,4 +15,3 @@ export FULLENV="${ENVIRONMENT}-${version}"
 declare -a rpms_to_remove=( "openssh-clients" "openssh-server" "openssh" )
 declare -a replace_from_apps=( "ucx/1.14.0" )
 declare -a outside_commands_to_include=( "pbs_tmrsh" "ssh" )
-declare -a outside_files_to_copy=()
